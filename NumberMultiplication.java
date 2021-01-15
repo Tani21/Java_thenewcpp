a35071519e92f9bf77a3b8cd967d0598b701d845
Tanishka Vaswani <tanishkavaswani1@gmail.com>
2021-01-15 13:11:59 +0530

    Printing table for given number

---
+++ NumberMultiplication.java
@@ -0,0 +1,12 @@
+public class NumberMultiplication
+{
+	public static void main(String[] args) {
+		int i = Integer.parseInt(args[0]);
+		
+		for(int j=1; j<11; j++)
+		{
+		    System.out.println(i+"*"+j+"="+(i*j));
+		}
+	}
+}
+
