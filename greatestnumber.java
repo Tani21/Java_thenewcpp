2fe2ddcf58079f62a025351c0e4bea6c403e2fd2
Tani21 <tanishkavaswani1@gmail.com>
2021-01-21 23:27:24 +0530

    find the greatest number out of three numbers

---
+++ greatestnumber.java
@@ -0,0 +1,23 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    int a=10, b=20, c=30;
+	    
+	    if(a>b && a>c)
+	    {
+	        System.out.println(a+ " is the greatest number");
+	    }
+	    
+	    if(b>a && b>c)
+	    {
+	        System.out.println(b+ " is the greatest number");
+	    }
+	    
+	    if(c>a && c>b)
+	    {
+	        System.out.println(c+ " is the greatest number");
+	    }
+		
+		
+	}
+}
