62407c9a8fe825ac388b008ab0795137e3522f5a
Tani21 <53483742+Tani21@users.noreply.github.com>
2021-03-02 23:37:02 +0530

    code reusability

---
+++ codereusability.java
@@ -0,0 +1,16 @@
+public class Main {
+
+  // method defined
+  private static int getSquare(int x){
+    return x * x;
+  }
+
+  public static void main(String[] args) {
+    for (int i = 1; i <= 5; i++) {
+
+      // method call
+      int result = getSquare(i);
+      System.out.println("Square of " + i + " is: " + result);
+    }
+  }
+}
