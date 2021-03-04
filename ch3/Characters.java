424c550d24e3103d2fbf65413230a93080151f6e
Tanishka Vaswani <tanishkavaswani1@gmail.com>
2021-03-04 18:37:21 +0530

    working with characters

---
+++ ch3/Characters.java
@@ -0,0 +1,15 @@
+
+public class Characters {
+
+	public static void main(String[] args) {
+		char chone = 88;
+		char chtwo ='A';
+		
+		System.out.println("chone: "+chone+" chtwo: "+chtwo);
+		chone++;chtwo++;
+		System.out.println("chone: "+chone+" chtwo: "+chtwo);
+
+	}
+
+}
+
