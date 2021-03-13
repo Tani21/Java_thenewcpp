e3728348226d16a945f0177a7e308814ce3c2c13
Tanishka Vaswani <tanishkavaswani1@gmail.com>
2021-03-13 14:26:16 +0530

    Solving questions on hackerrank

---
+++ hackerrank/SubArrays.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+class Calculation
+{
+    void printSubArray(int arr[])
+ {
+     int count=0;
+     int n=arr.length;
+    for(int j=0;j<n;j++)
+    {
+        int sum=0;
+        for(int k=j;k<n;k++)
+        {
+            sum=arr[k]+sum;
+            if(sum<0)
+            {
+                count++;
+            }
+        }
+    }
+    System.out.println(count);  
+ }
+}
+    
+
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int length = sc.nextInt();
+        int arr[] = new int[length];
+        
+        for(int i=0; i<arr.length; i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        
+        sc.close();
+        Calculation obj = new Calculation();
+        obj.printSubArray(arr);
+        
+        
+    }
+}
+
