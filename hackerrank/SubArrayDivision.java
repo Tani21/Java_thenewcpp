b53090f9704c82105fcc1c193a6b41048f567412
Tanishka Vaswani <53483742+Tani21@users.noreply.github.com>
2022-01-16 12:44:56 +0530

    Create SubArrayDivision.java

---
+++ hackerrank/SubArrayDivision.java
@@ -0,0 +1,73 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'birthday' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY s
+     *  2. INTEGER d
+     *  3. INTEGER m
+     */
+
+    public static int birthday(List<Integer> s, int d, int m) {
+    // Write your code here
+    int no_of_ways=0;
+
+    
+    for(int i=0; i<(s.size()-(m-1)); i++){
+        int current_sum=0;
+        for(int j=i; j<i+m; j++ ){
+            current_sum=current_sum+s.get(j);
+        }
+        
+        if(current_sum==d){
+            no_of_ways=no_of_ways+1;
+        }
+    }
+    
+    return no_of_ways;
+
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        String[] sTemp = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        List<Integer> s = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int sItem = Integer.parseInt(sTemp[i]);
+            s.add(sItem);
+        }
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int d = Integer.parseInt(firstMultipleInput[0]);
+
+        int m = Integer.parseInt(firstMultipleInput[1]);
+
+        int result = Result.birthday(s, d, m);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
