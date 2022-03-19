7cea943fe18017593973d8a04eee7990185fc4df
Tanishka Vaswani <53483742+Tani21@users.noreply.github.com>
2022-03-19 21:19:05 +0530

    Create arithmeticOperation.sh

---
+++ Placement/Bash/arithmeticOperation.sh
@@ -0,0 +1,14 @@
+Given two integers, x and y, find their sum, difference, product, and quotient.
+
+read x
+read y
+
+sum=$((x+y))
+difference=$((x-y))
+mul=$((x*y))
+div=$((x/y))
+
+echo $sum
+echo $difference
+echo $mul
+echo $div
